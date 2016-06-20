961aab64cd829e13394601cd6a13f7c5be9d7d69
Hai Nguyen <hainm.comp@gmail.com>
2016-06-20 16:36:11 -0400

    fix parse_data.py

--- scripts/parse_data.py
+++ scripts/parse_data.py
@@ -8,10 +8,14 @@ def main(residue):
     data = MMPBSA_API.load_mmpbsa_info('_MMPBSA_info')
     decomp_data = data['decomp']['gb']['complex']['TDC']
     
-    # data for residue 1
-    print(decomp_data[1])
+    # data for given residue
+    print(decomp_data[residue])
+
+    # data for all residues
+    # print(decomp_data)
 
 if __name__ == '__main__':
     # get residue number (index starts from 1)
-    residue = sys.argv[1]
+    residue = int(sys.argv[1])
+
     main(residue)
