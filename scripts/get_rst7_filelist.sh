4b66667e0e9be52ddfef7341eb583c5a7a50beff
Hai Nguyen <hainm.comp@gmail.com>
2016-06-20 16:29:34 -0400

    add filelist

--- scripts/get_rst7_filelist.sh
+++ scripts/get_rst7_filelist.sh
@@ -1,18 +1,5 @@
-import sys
+#!/bin/sh
 
-def get_filelist(pdb_folder):
-    filelist = None
-    with open(pdb_folder + '/_MMPBSA_info') as fh:
-        for line in fh:
-            if line.startswith("FILES.mdcrd"):
-                filelist = eval(line.split('=')[-1])
-    return filelist
+folder=$1
 
-if __name__ == '__main__':
-    """
-    Examples
-
-    python scripts/get_rst7_filelist.py 1be908_wt
-    """
-    for pdb in get_filelist(sys.argv[1]):
-        print(pdb)
+grep "as Amber NC Restart" $folder/cpptraj.log
